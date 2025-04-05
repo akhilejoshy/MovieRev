e048154ec644c88da92be99c7773e44a599aea91
akhilejoshy <akhilejoshy@gmail.com>
2025-04-06 02:07:40 +0530

    add requrement

--- MovieRev/settings.py
+++ MovieRev/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,6 +137,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -147,3 +152,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'akhilejoshy@gmail.com'
 EMAIL_HOST_PASSWORD = 'vyet eejv fbdp itvf'
+
+
+DEBUG = False  # for production
+ALLOWED_HOSTS = ['*']
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MovieRev.wsgi:application
